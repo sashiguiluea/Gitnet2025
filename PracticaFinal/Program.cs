8fb81c4a07530fffe477a37b606f8c9710738a97
SEGUNDO ANDRES SHIGUI LEMA <sa.shiguil@uea.edu.ec>
2025-06-05 13:04:32 -0500

    Github

--- PracticaFinal/Program.cs
+++ PracticaFinal/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Programador Develop Csharp Andrés Shigui Lema .Net!");
+Console.WriteLine("Programador Develop Csharp Andrés Shigui Lema. GitHub!");
